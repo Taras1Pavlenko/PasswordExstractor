4abf09261ac8396f5115f3f29905402c72cf5c6b
Тарас Павленко <taras.pavlenko@nure.ua>
2019-04-19 16:09:41 +0300

    Some changes

--- ITCloudAcademy.ReflectionQuest.PasswordSeeker/AssemblyExtractor.cs
+++ ITCloudAcademy.ReflectionQuest.PasswordSeeker/AssemblyExtractor.cs
@@ -12,17 +12,17 @@ namespace ITCloudAcademy.ReflectionQuest.PasswordSeeker
     class AssemblyExtractor
     {
         private string pathToDll = Path.GetFullPath("..\\..\\ITCloudAcademy.ReflectionQuest.Password.dll");
-
         private string password = String.Empty;
+        private List<Type> types = new List<Type>();
+        private SortedList<int, object> AllMembers = new SortedList<int, object>();
 
         public void Run()
         {
             GetNecessaryTypes();
         }
 
-        public void GetNecessaryTypes()
+        private void GetNecessaryTypes()
         {
-            List<Type> types = new List<Type>();
             Assembly assembly = Assembly.LoadFrom(pathToDll);
             Type[] allTypes = assembly.GetTypes();
             foreach (var type in allTypes)
@@ -32,8 +32,35 @@ namespace ITCloudAcademy.ReflectionQuest.PasswordSeeker
                     types.Add(type);
                 }
             }
+            GetGetNecessaryMembers();
         }
 
+        private void GetGetNecessaryMembers()
+        {
+            foreach (var type in types)
+            {
+                FieldInfo[] memberInfos = type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
 
+                foreach (var member in memberInfos)
+                {
+                    if ((Attribute.GetCustomAttribute(member, typeof(PasswordIsHereAttribute), false) != null))
+                    {
+                        PasswordIsHereAttribute password = (PasswordIsHereAttribute)Attribute
+                            .GetCustomAttribute(member, typeof(PasswordIsHereAttribute));
+                        int number = password.ChunkNo;
+                        AllMembers.Add(number, CreateInstance(type, member));
+                        Console.WriteLine(member);
+                    }
+                }
+            }
+        }
+
+        private object CreateInstance(Type type, FieldInfo member)
+        {
+            var a = type.Name;
+            var inctance = Activator.CreateInstance(type.GetType());
+            
+           return member.GetValue(type);
+        }
     }
 }
