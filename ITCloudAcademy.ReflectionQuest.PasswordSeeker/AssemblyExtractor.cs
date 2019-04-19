2ba110e1acfe7dc19fdc84df1d69e174808a37f2
Тарас Павленко <taras.pavlenko@nure.ua>
2019-04-19 08:24:09 +0300

    Creating a class to work with the assembly

---
+++ ITCloudAcademy.ReflectionQuest.PasswordSeeker/AssemblyExtractor.cs
@@ -0,0 +1,39 @@
+﻿using ITCloudAcademy.ReflectionQuest.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITCloudAcademy.ReflectionQuest.PasswordSeeker
+{
+    class AssemblyExtractor
+    {
+        private string pathToDll = Path.GetFullPath("..\\..\\ITCloudAcademy.ReflectionQuest.Password.dll");
+
+        private string password = String.Empty;
+
+        public void Run()
+        {
+            GetNecessaryTypes();
+        }
+
+        public void GetNecessaryTypes()
+        {
+            List<Type> types = new List<Type>();
+            Assembly assembly = Assembly.LoadFrom(pathToDll);
+            Type[] allTypes = assembly.GetTypes();
+            foreach (var type in allTypes)
+            {
+                if ((Attribute.GetCustomAttribute(type, typeof(IgnoreMeAttribute), false) == null))
+                {
+                    types.Add(type);
+                }
+            }
+        }
+
+
+    }
+}
--- ITCloudAcademy.ReflectionQuest.PasswordSeeker/Program.cs
+++ ITCloudAcademy.ReflectionQuest.PasswordSeeker/Program.cs
@@ -10,66 +10,10 @@ namespace ITCloudAcademy.ReflectionQuest.PasswordSeeker
     {
         static void Main()
         {
-            string pathToDll = Path.GetFullPath("..\\..\\ITCloudAcademy.ReflectionQuest.Password.dll");
-
-            string password = string.Empty; // TODO: Write password seeker by using reflection
-
-            List<Type> appropriateTypes = new List<Type>();
-
-            Assembly assembly = Assembly.LoadFrom(pathToDll);
-
-            Type[] types = assembly.GetTypes();
-
-            appropriateTypes = GetAppropriateTypes(types, nameof(IgnoreMeAttribute));
-
-
-            CreateInstanceOf(types);
+            AssemblyExtractor assemblyExtractor = new AssemblyExtractor();
+            assemblyExtractor.Run();
 
             Console.ReadKey();
-            Console.WriteLine(password);
-        }
-
-
-        public static List<Type> GetAppropriateTypes(Type[] typesArray, string attributeType)
-        {
-            List<Type> types = new List<Type>();
-            foreach (var type in typesArray)
-            {
-                object[] attrbs = type.GetCustomAttributes(false);
-                if (attrbs.Length == 0)
-                {
-                    types.Add(type);
-                }
-                else
-                {
-                    foreach (var attrb in attrbs)
-                    {
-                        if (!(attributeType == attrb.GetType().Name))
-                        {
-                            types.Add(type);
-                        }
-                    }
-                }
-            }
-            return types;
-        }
-
-        public static void CreateInstanceOf(Type[] types)
-        {
-            foreach (var obj in types)
-            {
-                var instanceOf = Activator.CreateInstance(obj.GetType());
-                WorkWithObject(instanceOf);
-            }
-        }
-
-        private static void WorkWithObject(object instanceOf)
-        {
-            MemberInfo[] memberInfos = instanceOf.GetType().GetMembers();
-            foreach (var item in memberInfos)
-            {
-                Console.WriteLine(item);
-            }
         }
     }
 }
